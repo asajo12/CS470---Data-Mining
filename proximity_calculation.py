1bf7b45cf2e0ec6b4056fba948cd33ddefa9052c
asajo12 <122995990+asajo12@users.noreply.github.com>
2024-09-10 17:07:11 -0400

    Initial commit

---
+++ proximity_calculation.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as py 
+
+#read csv file
+csvFile = pd.read_csv('data.csv')
+#print(csvFile)
+
+#1. get rid of non-numerical attributes
+
+
+#2. calculate the proximity for all the pairs of data instances 
